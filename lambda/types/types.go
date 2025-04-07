04a84fad723ae6270a8928b10b0f53f1bc055e43
Apostolos Papas <56194423+DevelopWithAP@users.noreply.github.com>
2025-04-07 09:02:45 +0300

    add password hashing

--- lambda/types/types.go
+++ lambda/types/types.go
@@ -1,6 +1,34 @@
 package types
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Username string `json:"username"`
+	HashedPassword string `json:"password"`
+}
+
 type RegisterUser struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+func NewUser(register RegisterUser) (User, error) {
+	hashedPassword, error := bcrypt.GenerateFromPassword([]byte(register.Password), 10)
+	
+	if error != nil {
+		return User{}, error
+	}
+
+	return User{
+		Username: register.Username,
+		HashedPassword: string(hashedPassword),
+	}, nil 
+}
+
+func ValidatePassword(hashedPassword, plaintextPassword string) bool {
+	error := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plaintextPassword))
+
+	return error == nil
 }
